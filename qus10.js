3d268e43390855647d1605ee72cd80515d1b5932
ANITJOHNSAJI <anitjohnsaji41@gmail.com>
2025-07-18 12:45:06 +0530

    created

--- qus10.js
+++ qus10.js
@@ -1,3 +1,12 @@
-let number=5;
-if(number%2==0) evenNumber : oddNumber;
-console.log("")
+// What is the ternary operator and when should you use it?
+// Use the ternary operator to assign a value based on whether a number is even or odd.
+
+/* Answer */
+
+// Terinary Operator is used to check a condition and execute a block of code if the condition is true.
+
+let number=3;
+let answer=number==0? "the number is zero":"the number is not zero";
+    console.log(answer);
+let result=number>0? "The number is positive":"the number is negative";
+console.log(result);

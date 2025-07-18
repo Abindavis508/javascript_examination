2d2b00288ff1d4c6f51745e6b34d379cfc70d867
ANITJOHNSAJI <anitjohnsaji41@gmail.com>
2025-07-18 12:51:33 +0530

    files added

---
+++ qus4.js
@@ -0,0 +1,14 @@
+// What is the difference between == and ===?
+// Write a function that compares two values using both == and === and logs the result.
+
+
+/* Answer */
+
+// == is used to check the equality of two values
+// === is used to check the equality of two values and also check the data type    
+
+let a=4;
+let b="4";
+
+console.log(a==b);
+console.log(a===b);

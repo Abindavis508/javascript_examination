b098f93a052d32cd6c2491d2ac02332d76e71fe3
ANITJOHNSAJI <anitjohnsaji41@gmail.com>
2025-07-18 12:58:36 +0530

    files

---
+++ qus1.js
@@ -0,0 +1,26 @@
+// What are the differences between var, let, and const?
+// Declare a variable using each and try reassigning and redeclaring them. Observe and explain the results.
+
+/* Answer */
+
+
+// var is used to declare a variable.
+// let is used to declare a block scoped variable.
+// const is used to declare a constant variable.
+
+
+
+var a=10;
+console.log(a);
+a=20;
+console.log(a);
+
+
+let b=10;
+console.log(b);
+b=20;
+console.log(b);
+
+
+const c=10;
+console.log(c);

3249b2cc5b9e62d3d46d8e41939dffc9d7c554f9
ANITJOHNSAJI <anitjohnsaji41@gmail.com>
2025-07-18 12:33:04 +0530

    files created

---
+++ qus10.js
@@ -0,0 +1,3 @@
+let number=5;
+if(number%2==0) evenNumber : oddNumber;
+console.log("")
---
+++ qus3.js
@@ -0,0 +1,32 @@
+// Describe the difference between if, else if, and switch.
+// Write a program that checks a number and prints whether it’s positive, negative, or zero using both if and switch.
+// 
+
+// /*  answer */
+
+// In if and switch, we check a condition and execute a block of code if the condition is true.
+// In else if, we check multiple conditions and execute a block of code if the condition is true.
+// In switch we check multiple conditions and execute a block of code if the condition is true.
+
+let number=0;
+if (number===0){
+    console.log("the number is zero");
+}else if(number>0){
+    console.log("the number is positive");
+}else if(number<0){
+    console.log("the number is negavite");
+}
+
+// let num=8;
+// switch(num){
+//     case 0:
+//         console.log("the number is zero");
+//         break;
+//     case num>0:
+//         console.log("the number is positive");
+//         break;
+//     case num<0:
+//         console.log("the number is negavite");
+//         break;
+// }
+
